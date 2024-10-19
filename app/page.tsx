a1409f10820cbd7ec1d556598c60a3c3292d51da
Akash Srivastava <akashniki@gmail.com>
2024-10-19 21:21:18 +0545

    website: Some smoll fixes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import AboutSection from "./components/AboutSection";
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 import HomeSection from "./components/HomeSection";
+import History from "./history/page";
 
 export default function Home() {
   return (
     <div>
       <HomeSection />
+      <History />
       <AboutSection />
       <ContactSection />
 
