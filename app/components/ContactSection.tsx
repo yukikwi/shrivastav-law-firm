5c1dcfa91c0db8d19bc595ed1833f081989af63a
yukikwi <pachara2202@gmail.com>
2024-10-26 22:14:35 +0700

    fix: small text size in mobile

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -5,7 +5,7 @@ const ContactSection = React.forwardRef((props, ref:React.ForwardedRef<HTMLEleme
     <section ref={ref} id="contact-us" className="p-8">
       <h2 className="text-5xl font-bold">CONTACT US</h2>
 
-      <span className="block my-12 text-8xl font-medium">
+      <span className="block my-12 text-4xl md:text-8xl font-medium">
         Let’s connect and get to know each other
       </span>
     </section>
